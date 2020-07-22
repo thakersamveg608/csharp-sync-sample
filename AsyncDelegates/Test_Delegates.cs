ac696a50a17196b7ada77dbb4bb2962e6f323c84
Zenithec Techware <zenithec.ltd@gmail.com>
2020-07-22 11:35:38 +0530

    background

---
+++ AsyncDelegates/Test_Delegates.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace AsyncDelegates
+{
+    public class UnitTest1
+    {
+        private void DoWork()
+        {
+            Debug.WriteLine("hello world");
+            Debug.WriteLine(System.Threading.Thread.CurrentThread.ManagedThreadId.ToString());
+        }
+
+        delegate void DoWorkDelegate();
+
+        [Fact]
+        public void Demo01()
+        {
+            Debug.WriteLine(System.Threading.Thread.CurrentThread.ManagedThreadId.ToString());
+            DoWorkDelegate doWorkDelegate = new DoWorkDelegate(DoWork);
+            //doWorkDelegate();
+            AsyncCallback callback = new AsyncCallback(Callback);
+            IAsyncResult ar = doWorkDelegate.BeginInvoke(callback, doWorkDelegate);
+        }
+
+        private static void Callback(IAsyncResult ar)
+        {
+            var doWorkDelegate = ar.AsyncState as DoWorkDelegate;
+            doWorkDelegate.EndInvoke(ar);
+        }
+    }
+}
